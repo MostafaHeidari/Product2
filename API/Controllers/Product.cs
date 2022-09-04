1972295d5796920aacb5f11e53297b7aa9c75b38
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-09-04 13:35:33 +0200

    Add project files.

---
+++ API/Controllers/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class ProductController : ControllerBase
+{
+    [HttpGet]
+    public object HelloWorld()
+    {
+        return "Hello World";
+    }
+}
