b4ab1b5da959c77cd64ce1bec17f86e41d7afa73
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-09-04 14:47:18 +0200

    Infrastructure knows about Entites class.cs now . its a dependencise

---
+++ Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int price { get; set; }
+        
+    }
+}
